cd483f575306099039d06818ed6a7f523366d8ac
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-19 09:36:11 -0400

    scope lab done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"; 
+var bestCustomer;
+function getCustomerName() {
+    return customerName;
+}
+
+  function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+  bestCustomer = "not bob"; 
+}
+  
+function overwriteBestCustomer(name) {
+   bestCustomer = name;
+}
+  
+const leastFavoriteCustomer = "Karen";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Chad"; 
+}
